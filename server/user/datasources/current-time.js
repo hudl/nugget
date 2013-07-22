efbf67e9bd57f6ce28a377d2e711c82750fbe1c5
Rob Hruska <robhruska@gmail.com>
2013-07-22 14:28:28 -0500

    Inline documentation on current-time example

--- server/user/datasources/current-time.js
+++ server/user/datasources/current-time.js
@@ -2,7 +2,32 @@ var log = require('../../core/common').log(__filename);
 
 var interval;
 
+// A datasource module must export two functions: start and stop.
+// start() is invoked when the datasource needs to be initialized, and
+// stop() should clean up any connections or resources that the datasource
+// is responsible for.
+
 module.exports = {
+
+	// The first attribute to start() is an object that will contain what's
+	// defined for this datasource in server-config.json. For this specific
+	// example of current-time, if server-config.json contained the following:
+	//
+	// {
+	//     "datasources": {
+	//         "current-time": {
+	//             "format": "time"
+	//         }
+	//     }
+	// }
+	//
+	// Then the config object here would be:
+	//
+	// { "format": "time" }
+
+	// The second attribute to start() is an emitter that's provided by the
+	// server. The emitter provides one available method, emitStat(), that
+	// datasources use to make their stats available to displays.
 	start: function (config, emitter) {
 
 		function getTime(format) {
@@ -24,9 +49,22 @@ module.exports = {
 			}
 		}
 
+		// In this case we're simulating a polled resource. However, a
+		// datasource might work in any number of ways. It may poll and scrape
+		// a web page, or it might connect to a streaming API. The implementation
+		// doesn't matter much.
+
 		interval = setInterval((function run () {
 
 			var formatted = getTime(config.format);
+
+			// This makes the 'current-time' stat available for displays to use.
+			// The message structure has a couple of requirements:
+			// - It must be an object
+			// - It shouldn't use a property name of "source", since the server
+			//   will automatically add that before emitting the message to
+			//   clients.
+
 			emitter.emitStat('current-time', {
 				value: formatted
 			});
--- server/user/displays/current-time.js
+++ server/user/displays/current-time.js
@@ -1,3 +1,4 @@
+// Displays are defined as require.js modules.
 define(function () {
 
 	function initialize (socket) {
@@ -8,11 +9,33 @@ define(function () {
 		$('#time').text(message.value);
 	}
 
+	// A display module should return an object that defines what stats
+	// (i.e. sources) it needs. The module can (and probably should) define a
+	// couple of event callbacks (see below).
+
 	return {
+
+		// A list of stats (sources) this module wants to consume. The values
+		// here will match the value that a datasource module passes as the first
+		// argument to emitter.emitStat().
 		sources: [
 			'current-time'
 		],
+
+		// Callback to invoke when a new stat is received. The callback will be
+		// passed the stat's message object. The message object will also contain
+		// a new property, source, that's the stat's source name (e.g.
+		// "current-time").
+		// 
+		// This callback should be the one that manipulates the page to update
+		// or re-display the new information.
+		//
+		// If the display uses multiple sources, the callback can switch on the
+		// "source" property of the message.
 		onStat: onStat,
+
+		// This callback will be invoked whenever this display is loaded. It will
+		// be passed the socket.io object used for communicating with the server.
 		initialize: initialize
 	}
 
